66c2574dba5f2e58b34ffe7a068ead6d370907c1
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-04-07 22:31:45 +0530

    Add files via upload

---
+++ Positive Or Negative With if else.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+     int a;
+     printf("\nEnter Any Number=");
+     scanf("%d",&a);
+     if(a>0)
+     {
+         printf("\nThe Number is Positive");
+     }
+     else
+     {
+         printf("\nThe Number is Negative");
+     }
+     getch();
+     clrscr();
+} 
