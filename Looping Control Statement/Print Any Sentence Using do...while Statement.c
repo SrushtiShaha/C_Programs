99bb015ab408c770f629bd974608a707b31cde02
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-04-17 21:49:00 +0530

    Add files via upload

---
+++ Looping Control Statement/Print Any Sentence Using do...while Statement.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+        int N = 0;
+        
+        do
+        {
+              printf("\n India ");
+              N++;
+        }while(N < 5);
+        
+        printf("\n\n\t Process Finished ");
+  
+        getch();
+        return 0;
+}
