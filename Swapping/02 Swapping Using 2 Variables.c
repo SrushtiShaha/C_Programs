ab06820f42a8d52ad160fa644ba8d57532dc4864
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-04-17 22:33:29 +0530

    Add files via upload

---
+++ Swapping/02 Swapping Using 2 Variables.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+      int No1 = 0, No2 = 0, Temp = 0;
+      
+      printf("\n Enter Two Numbers -> ");
+      scanf("%d%d",&No1,&No2);
+      
+      printf("\n Before Swapping\n\n No1 is %d And No2 is %d",No1,No2);
+      
+      No1 = No1 * No2;
+      No2 = No1 / No2;
+      No1 = No1 / No2;
+      
+      printf("\n\n After Swapping -> \n\n First Number Is %d And Second Number Is %d",No1,No2);
+      
+      getch();
+      return 0;
+}
