958c240292babe95aecc795165a2e59a556a9a2d
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-04-28 21:44:45 +0530

    Add files via upload

---
+++ Function/Using Function Maximum From Two Numbers.c
@@ -0,0 +1,30 @@
+// Accept Something Return Something
+
+#include <stdio.h>
+#include <conio.h>
+
+int Max(int, int);
+
+int main()
+{
+      int No1 = 0, No2 = 0;
+      
+      printf("\n Enter Any Two No. => ");
+      scanf("%d%d",&No1,&No2);
+      
+      printf("\n Maximum Number Is => %d",Max(No1,No2));
+      
+      getch();
+      return 0;
+}
+int Max(int N1, int N2)
+{
+      if(N1 > N2)
+      {
+            return N1;
+      }
+      else
+      {
+            return N2;
+      }
+}
