2a56e7e6f6f7039fb1da67cfa42a7e7430c27202
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-05-02 23:56:27 +0530

    Add files via upload

---
+++ Function/Through Function Find Number Is Even Or Odd.c
@@ -0,0 +1,36 @@
+// Accept Nothing Return Nothing.                        
+
+#include <stdio.h>
+#include <conio.h>
+
+void Even();
+
+int main()
+{
+      Even();
+      
+      getch();
+      return 0;
+}
+
+void Even()
+{
+        int No = 0;
+        
+        printf("\n Enter Any Number => ");
+        scanf("%d",&No);
+        
+        if( No == 0 )
+        {
+              printf(" \n Given Number %d Is Neutral. ", No );
+        } 
+        else if( No % 2 == 0 )
+        {
+              printf(" \n Given Number %d Is Even. ", No );
+        }
+        else
+        {
+              printf(" \n Given Number %d Is Odd. ", No );
+        }       
+        
+}
