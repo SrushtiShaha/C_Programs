da4c8eff8beb96d271b9bccb6aae495829c0f675
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-05-09 00:41:18 +0530

    Add files via upload

---
+++ Array/Find Second Maximum Array Element.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+        int Arr[5] = {};
+        int i = 0, Max1 = 0, Max2 = 0;
+        
+        printf(" Enter Any Five Array Elements To Find Second Maximum Element \n");
+        
+        for( i = 0; i < 5; i++ )
+        {
+                  printf("\n Enter %d Element => ", i+1);
+                  scanf("%d",&Arr[i]);
+        }
+        
+        getch();
+        
+        for( i = 0; i < 5; i++ )
+        {
+                  if( Arr[i] > Max1 )
+                  {
+                          Max1 = Arr[i];
+                  }
+        }
+        
+        for( i = 0; i < 5; i++ )
+        {
+                  if( Arr[i] > Max2 && Arr[i] != Max1 )
+                  {
+                          Max2 = Arr[i];
+                  }
+        }
+        
+        printf("\n\n Second Maximum Element In Array Is => %d", Max2);
+        
+        getch();
+        return 0;
+}
