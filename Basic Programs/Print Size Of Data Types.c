f6bf568e6105222ef79849e26d13c74dc5f5ec11
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-05-25 23:55:51 +0530

    Add files via upload

---
+++ Basic Programs/Print Size Of Data Types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+          printf("\n Size Of int Is         => %d",sizeof(int));
+          printf("\n Size Of long int Is    => %d",sizeof(long int));
+          printf("\n Size Of short int Is   => %d\n",sizeof(short int));
+          printf("\n Size Of char Is        => %d\n",sizeof(char));
+          printf("\n Size Of float Is       => %d",sizeof(float));
+          printf("\n Size Of double Is      => %d",sizeof(double));
+          printf("\n Size Of long double Is => %d\n",sizeof(long double));
+          printf("\n Size Of void Is        => %d\n",sizeof(void));
+          
+          getch();
+          return 0;
+}
