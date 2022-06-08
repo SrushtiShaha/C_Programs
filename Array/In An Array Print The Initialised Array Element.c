3d95340aecfb7c1348d12d91b95b916a5559b426
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-06-08 22:37:03 +0530

    Add files via upload

---
+++ Array/In An Array Print The Initialised Array Element.c
@@ -0,0 +1,18 @@
+// Using printf() just Print The Value Of Initialised Array Element.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+              int Arr[5] = { 47, 98, 29 };
+              
+              printf("\n Value Of 1st Element => %d. ",Arr[0]);
+              printf("\n Value Of 2nd Element => %d. ",Arr[1]);
+              printf("\n Value Of 3rd Element => %d. ",Arr[2]);
+              printf("\n Value Of 4th Element => %d. ",Arr[3]);
+              printf("\n Value Of 5th Element => %d. ",Arr[4]);
+              
+              getch();
+              return 0;
+}
