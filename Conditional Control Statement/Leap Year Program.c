9b2a51392e7755d91fa406d4f3fce7dd8de1303e
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-06-09 17:36:01 +0530

    Update Leap Year Program.c

--- Conditional Control Statement/Leap Year Program.c
+++ Conditional Control Statement/Leap Year Program.c
@@ -3,18 +3,19 @@
 
 int main()
 {
-      int year;
-      printf("\n\nEnter Any Year");
+      int year = 0;
+
+      printf("\n\n Enter Any Year");
       scanf("%d",&year);
   
       if(year % 4 == 0)
       {
-           printf("\nThe Year is Leap Year");
+           printf("\n The Year is Leap Year");
       }
    
       else
       {
-           printf("\nThe Year is Not Leap Year");
+           printf("\n The Year is Not Leap Year");
       }
 
        getch();
