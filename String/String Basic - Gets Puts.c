78fb40fcaa1ba4ffe3c064de56afbd4b390e92a4
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-06-15 23:56:25 +0530

    Add files via upload

---
+++ String/String Basic - Gets Puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+          char cArr[50] = {'\0'};
+          
+          puts("\n Enter A String => ");
+          gets(cArr);
+          
+          puts("\n Given String Is => ");
+          puts(cArr);
+          
+          getch();
+          return 0;
+}
