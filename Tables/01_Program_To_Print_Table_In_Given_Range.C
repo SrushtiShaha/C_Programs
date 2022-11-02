e605d0fcef97362db3a2644358e81214037323d1
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-11-03 00:20:28 +0530

    Program_Of_Table

---
+++ Tables/01_Program_To_Print_Table_In_Given_Range.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int SP,EP,Cnt,NO;
+    printf("\n\t Enter First Number =>  ");
+    scanf("%d",&SP);
+   
+    printf("\n\t Enter Last Number =>  ");
+    scanf("%d",&EP);
+   
+    for(NO = 1; NO <= 10; NO++)
+    {
+        for(Cnt = SP; Cnt <= EP; Cnt++)
+        {
+            printf(" %-3d",NO * Cnt);
+        }
+
+        printf("\n");
+     }
+   
+    getch();
+    return 0;
+}
