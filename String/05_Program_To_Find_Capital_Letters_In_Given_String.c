782eee672055c27b29b3418bd42d1c0bc459d66f
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-04-11 23:30:10 +0530

    Program_Of_String

---
+++ String/05_Program_To_Find_Capital_Letters_In_Given_String.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        char cSrc[20] = {'\0'};
+        int i = 0, iCnt = 0;
+
+        printf("\n Enter A String -> ");
+        gets(cSrc);
+
+        while( cSrc[i] != '\0' )
+        {
+                if( cSrc[i] >= 'A' && cSrc[i] <= 'Z' )
+                {
+                        iCnt++;
+                }
+
+                i++;
+        }
+
+        printf("\n Capital Letters In Given String %s Is -> %d \n", cSrc, iCnt);
+
+        getch();
+        return 0;
+}
